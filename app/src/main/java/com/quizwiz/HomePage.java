37f85ba42f9efac10693a7e155c2cc98c32ac591
miteshpatekar <miteshpatekar29@gmail.com>
2015-03-13 18:51:49 -0500

    Firebase Configuration added
    Sample Data Read done

--- app/src/main/java/com/quizwiz/HomePage.java
+++ app/src/main/java/com/quizwiz/HomePage.java
@@ -3,7 +3,15 @@ package com.quizwiz;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
 
 
 public class HomePage extends ActionBarActivity {
@@ -12,7 +20,54 @@ public class HomePage extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Firebase.setAndroidContext(this);
+
+        Firebase myFirebaseRef = new Firebase("https://popping-heat-8474.firebaseio.com/");
+       // Firebase rootRef = new Firebase("https://docs-examples.firebaseio.com/web/data");
+
+        Query queryRef = myFirebaseRef.orderByValue();
+        queryRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChildKey) {
+                System.out.println("The " + snapshot.getKey() + " dinosaur's score is " + snapshot.getValue());
+                Log.d("Msg","The " + snapshot.getKey() + " dinosaur's score is " + snapshot.getValue());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+            // ....
+        });
+
+        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue());
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
     }
+
+
 
     public void QuickQuiz(View v)
     {
